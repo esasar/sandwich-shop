960436adeeefb9aaac0fb17a2c8f2c852720910d
Esa Särkelä <esa.sarkela@tuni.fi>
2024-03-18 19:10:26 +0200

    add server-a stub

---
+++ backend/server-a/service/OrderService.js
@@ -0,0 +1,63 @@
+'use strict';
+
+
+/**
+ * Add an order for an sandwich
+ *
+ * order Order place an order for a sandwich
+ * returns Order
+ **/
+exports.addOrder = function(order) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {"empty": false};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Find an order by its ID
+ * IDs must be positive integers
+ *
+ * orderId Long ID of the order that needs to be fetched
+ * returns Order
+ **/
+exports.getOrderById = function(orderId) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {"empty": false};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Get a list of all orders. Empty array if no orders are found.
+ *
+ * returns ArrayOfOrders
+ **/
+exports.getOrders = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "blank": true,
+  "bytes": [],
+  "empty": true
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
