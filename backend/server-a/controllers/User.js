e18358e49be01e848191c49720452706aa740a72
Heidi Seppi <heidi.seppi@tuni.fi>
2024-04-04 04:49:11 +0300

    Created rest of the user endpoints. Some parts has been marked with todos because login system hasn't been created yet

--- backend/server-a/controllers/User.js
+++ backend/server-a/controllers/User.js
@@ -7,16 +7,16 @@ var User = require('../models/user.js');
 // TODO: Include API key when posting? 
 
 // Password is not encrypted now and this could be added later.
-// Creating a new user. If user with the same username already exists, gives an error.
+// Creates a new user. If user with the same username already exists, gives an error. 
+// Notice that this doesn't check if username, password and email are valid.
 userRouter.post('/', async (request, response) => {
   const body = request.body;
 
   // Should this check be done elsewhere?
-  const user = await User.findOne({ username: body.username }, 
-    { projection: { username: 1} });
+  const user = await User.findOne({ username: body.username });
   
   // Only creates a new user if user with the same username doesn't already exist.
-  if (user != null) {
+  if (user == null) {
     const new_user = new User({
       username: body.username,
       email: body.email,
@@ -44,8 +44,7 @@ userRouter.post('/login', async (request, response) => {
     ]
   });
 
-  if (user != null) {
-    response.body('Successfull login');
+  if (user !== null) {
     // TODO: log in to the system.
   }
   else {
@@ -59,5 +58,67 @@ userRouter.post('/logout', async (request, response) => {
 
 });
 
+// Get user by username.
+userRouter.get('/:username', async (request, response) => {
+  // First checks if username is valid.
+  if (!usernameIsValid(request.params.username)) {
+    response.status(400).json({ error: 'Invalid username supplied' });
+  }
+  else {
+    const user = await User.findOne({ username: request.params.username });
+
+    if (user !== null) {
+      response.json(user);
+    }
+    else {
+      response.status(404).json({ error: 'User not found' });
+    }
+  }
+});
+
+// Updates username. This can only be done by the logged in user.
+userRouter.put('/:username', async (request, response) => {
+  // TODO: checks if user is logged in.
+
+  // First checks if username is valid. 
+  if (!usernameIsValid(request.params.username)) {
+    response.status(400).json({ error: 'Invalid user supplied' });
+  }
+  else {
+    const body = request.body;
+    const user = await User.findOneAndUpdate({ username: body.username }, 
+    { username: request.params.username });
+
+    if ( user === null ) {
+      response.status(404).json({ error: 'User not found' });
+    }
+
+    response.end();
+  }
+});
+
+// Delete user. This can only be done by the logged in user.
+userRouter.delete('/:username', async (request, response) => {
+  // TODO: checks if user is logged in.
+  // First checks if username is valid. 
+  if (!usernameIsValid(request.params.username)) {
+    response.status(400).json({ error: 'Invalid username supplied' });
+  }
+  else {
+    const user = await User.findOneAndDelete({ username: request.params.username });
+
+    if ( user === null ) {
+      response.status(404).json({ error: 'User not found' });
+    }
+
+    response.end();
+  }
+});
+
+// Function checks if username is valid. 
+// Username can only consist of number, letter and _, ., - characters.
+function usernameIsValid(username) {
+  return /^[0-9a-zA-Z_.-]+$/.test(username);
+}
 
 module.exports = userRouter;
