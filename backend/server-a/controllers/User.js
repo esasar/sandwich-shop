19cfe13b92f3211247973cbf966c62eda21754e4
Heidi Seppi <heidi.seppi@tuni.fi>
2024-04-04 03:42:44 +0300

    Modified user controller to have post login and logout frames

--- backend/server-a/controllers/User.js
+++ backend/server-a/controllers/User.js
@@ -2,87 +2,62 @@
 
 const userRouter = require('express').Router();
 var utils = require('../utils/writer.js');
-// var User = require('../service/UserService');
 var User = require('../models/user.js');
 
 // TODO: Include API key when posting? 
-// Password is not encrypted now. Could be modified later.
+
+// Password is not encrypted now and this could be added later.
+// Creating a new user. If user with the same username already exists, gives an error.
 userRouter.post('/', async (request, response) => {
   const body = request.body;
-  const new_user = new User({
-    username: body.username,
-    email: body.email,
-    password: body.password,
-  });
 
-  const savedUser = await new_user.save();
-  response.json(savedUser);
+  // Should this check be done elsewhere?
+  const user = await User.findOne({ username: body.username }, 
+    { projection: { username: 1} });
+  
+  // Only creates a new user if user with the same username doesn't already exist.
+  if (user != null) {
+    const new_user = new User({
+      username: body.username,
+      email: body.email,
+      password: body.password,
+    });
+  
+    const savedUser = await new_user.save();
+    response.json(savedUser);
+  }
+  else {
+    // Not in the original sandwich API.
+    response.status(400).json({ error: 'User with the chosen username already exists' });
+  }
 });
 
-module.exports.createUser = function createUser (req, res, next) {
-  var body = req.swagger.params['body'].value;
-  User.createUser(body)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
+// TODO: Logging user in to the system.
+// Checks if user with same username and password can be found in the system.
+userRouter.post('/login', async (request, response) => {
+  const body = request.body;
+  const user = await User.findOne(
+    {
+    $and: [
+      { username: body.username},
+      { password: body.password},
+    ]
+  });
 
-module.exports.deleteUser = function deleteUser (req, res, next) {
-  var username = req.swagger.params['username'].value;
-  User.deleteUser(username)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
+  if (user != null) {
+    response.body('Successfull login');
+    // TODO: log in to the system.
+  }
+  else {
+    response.status(400).json({ error: 'Invalid username/password supplied' });
+  }
+});
 
-module.exports.getUserByName = function getUserByName (req, res, next) {
-  var username = req.swagger.params['username'].value;
-  User.getUserByName(username)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
+// TODO: Logs out the current user.
+userRouter.post('/logout', async (request, response) => {
+  // TODO: logout.
 
-module.exports.loginUser = function loginUser (req, res, next) {
-  var user = req.swagger.params['user'].value;
-  User.loginUser(user)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
+});
 
-module.exports.logoutUser = function logoutUser (req, res, next) {
-  User.logoutUser()
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
-
-module.exports.updateUser = function updateUser (req, res, next) {
-  var username = req.swagger.params['username'].value;
-  var body = req.swagger.params['body'].value;
-  User.updateUser(username,body)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
-};
 
 module.exports = userRouter;
